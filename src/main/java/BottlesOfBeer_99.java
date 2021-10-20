29b0828b3e1b1abf2a217d5583f3c73ec627371b
Yennefer1173 <92054459+Yennefer1173@users.noreply.github.com>
2021-10-20 17:52:32 +0300

    Доделала домашку

--- src/main/java/BottlesOfBeer_99.java
+++ src/main/java/BottlesOfBeer_99.java
@@ -1,2 +1,21 @@
 public class BottlesOfBeer_99 {
+
+    public static void bottlesOfBeer_99() {
+
+                /*99 bottles of beer on the wall, 99 bottles of beer.
+                Take one down and pass it around, 98 bottles of beer on the wall.*/
+
+        for (int i = 99; i > 1; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.\n" +
+            "Take one down and pass it around, " + (i - 1) + " bottles of beer on the wall.");
+            System.out.println();
+        }
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer.\n" +
+                "Take one down and pass it around, no more bottles of beer on the wall.");
+        System.out.println();
+        System.out.println("No more bottles of beer on the wall, no more bottles of beer.\n" +
+                "Go to the store and buy some more, 99 bottles of beer on the wall.");
+
+
+    }
 }
--- src/test/java/BottlesOfBeer_99Test.java
+++ src/test/java/BottlesOfBeer_99Test.java
@@ -1,2 +1,9 @@
+import org.junit.Test;
+
 public class BottlesOfBeer_99Test {
+
+    @Test
+    public void testBottlesOfBeer_99() {
+        BottlesOfBeer_99.bottlesOfBeer_99();
+    }
 }
