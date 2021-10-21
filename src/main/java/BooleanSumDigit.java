f3a91e9b9a909f94b07b2a94a4bf47e9673210f8
Yennefer1173 <92054459+Yennefer1173@users.noreply.github.com>
2021-10-21 23:15:21 +0300

    Доделала домашку

--- src/main/java/BooleanSumDigit.java
+++ src/main/java/BooleanSumDigit.java
@@ -1,5 +1,19 @@
 public class BooleanSumDigit {
 
+    /*Напишите метод, который принимает массив целых положительных чисел
+    (в массиве четное количество элементов),
+    и возвращает true, если сумма чисел в первой половине массива
+    больше суммы чисел во второй половине массива.
+    Иначе возвращается false
+
+    Test Data:
+    Input = {4, 6, 3, 7, 1, 2}
+    Expected Result = true
+
+    Input = {12, 48, 15, 125}
+    Expected Result = false*/
+
+
     public static boolean getBooleanSumDigit(int[] array) {
         int a = 0;
         int b = 0;
